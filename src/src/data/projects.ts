d6d83b8bc3e03fc4f0422204a0dc57872c96808c
Howl-Fang <69738986+Howl-Fang@users.noreply.github.com>
2026-04-09 11:17:05 +0800

    project update

--- src/src/data/projects.ts
+++ src/src/data/projects.ts
@@ -55,4 +55,43 @@ export const projects: Project[] = [
     },
     tags: ["WIP"],
   },
+  {
+    id: "project-4",
+    title: {
+      en: "Disable AWDL",
+      zh: "禁用 AWDL",
+    },
+    description: {
+      en: "Disables AWDL to optimize moonlight performance on macOS. This tool is designed to improve the performance of Moonlight streaming by disabling the Apple Wireless Direct Link (AWDL) protocol, which can interfere with network performance.",
+      zh: "禁用 AWDL 以优化 macOS 上的 Moonlight 性能。该工具旨在通过禁用 Apple Wireless Direct Link (AWDL) 协议来提高 Moonlight 流媒体的性能，该协议可能会干扰网络性能。",
+    },
+    tags: ["Shell Script", "macOS"],
+    github: "https://github.com/Howl-Fang/Disable-AWDL-for-Moonlight-Streaming-on-Mac",
+  },
+  {
+    id: "project-5",
+    title: {
+      en: "COMP2012H",
+      zh: "COMP2012H",
+    },
+    description: {
+      en: "Course COMP2012H, OOP of C++",
+      zh: "课程 COMP2012H，C++ 面向对象编程",
+    },
+    tags: ["C++", "OOP"],
+    github: "https://github.com/Howl-Fang/COMP2012H",
+  },
+  {
+    id: "project-6",
+    title: {
+      en: "Mix-m4s-from-bilibili-cache",
+      zh: "从 B 站缓存混合 m4s",
+    },
+    description: {
+      en: "A tool to merge m4s files from Bilibili cache into a single video file.",
+      zh: "一个将 B 站缓存中的 m4s 文件合并成单个视频文件的工具。",
+    },
+    tags: ["Python", "Bilibili"],
+    github: "https://github.com/Howl-Fang/Mix-m4s-from-bilibili-cache",
+  },
 ];
